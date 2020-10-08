c1249f1c8e16bf43f1acc2b98e40fcebf552cb01
dary-winata <dary.winata15@gmail.com>
2020-10-09 05:37:37 +0700

    Mencoba Commit ke Github

---
+++ app/src/main/java/pens/lab/app/belajaractivity/modul/profile/ProfileContract.java
@@ -0,0 +1,21 @@
+package pens.lab.app.belajaractivity.modul.profile;
+
+import android.content.Intent;
+
+import pens.lab.app.belajaractivity.base.BasePresenter;
+import pens.lab.app.belajaractivity.base.BaseView;
+
+/**
+ * Created by fahrul on 13/03/19.
+ */
+
+public interface ProfileContract {
+    interface View extends BaseView<Presenter> {
+//        void redirectToProfile();
+    }
+
+    interface Presenter extends BasePresenter {
+//        void performLogin(String email, String password);
+        String[] getValueIntent(Intent intent);
+    }
+}
